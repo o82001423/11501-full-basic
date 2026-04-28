866aeb4ef2460cb2ff1761570bc2d3e27bdf8d3f
d1611234 <d1611234@gmail.com>
2026-04-28 15:25:54 +0800

    更新

---
+++ 14-pra14.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html lang="zh-tw">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title反轉陣列</title>
+</head>
+<body>
+    <H2>請設計一支程式,在不產生新陣列的狀況下,將一個陣列的元素順序反轉(利用迴圈)</H2>
+    <P>例:$a=[2,4,6,1,8] 反轉後 $a=[8,1,6,4,2]</P>
+</body>
+</html>
